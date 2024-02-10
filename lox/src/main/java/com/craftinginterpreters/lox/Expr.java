fae2eaa6f5e7c6e1637a67d3c7d2c026a9395c0e
JP <joaobianchi@live.com>
2024-02-09 21:37:47 -0300

    created ast printer

--- lox/src/main/java/com/craftinginterpreters/lox/Expr.java
+++ lox/src/main/java/com/craftinginterpreters/lox/Expr.java
@@ -2,7 +2,37 @@ package com.craftinginterpreters.lox;
 
 import java.util.List;
 
-abstract class Expr {
+public abstract class Expr {
+  interface Visitor<R> {
+    R visitLiteralExpr(Literal expr);
+    R visitUnaryExpr(Unary expr);
+    R visitBinaryExpr(Binary expr);
+    R visitGroupingExpr(Grouping expr);
+  }
+  static class Literal extends Expr {
+    Literal(Object value) {
+      this.value = value;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitLiteralExpr(this);
+    }
+    final Object value;
+  }
+  static class Unary extends Expr {
+    Unary(Token operator, Expr right) {
+      this.operator = operator;
+      this.right = right;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitUnaryExpr(this);
+    }
+    final Token operator;
+    final Expr right;
+  }
   static class Binary extends Expr {
     Binary(Expr left, Token operator, Expr right) {
       this.left = left;
@@ -10,6 +40,10 @@ abstract class Expr {
       this.right = right;
     }
 
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitBinaryExpr(this);
+    }
     final Expr left;
     final Token operator;
     final Expr right;
@@ -19,22 +53,12 @@ abstract class Expr {
       this.expression = expression;
     }
 
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitGroupingExpr(this);
+    }
     final Expr expression;
   }
-  static class Literal extends Expr {
-    Literal(Object value) {
-      this.value = value;
-    }
 
-    final Object value;
-  }
-  static class Unary extends Expr {
-    Unary(Token operator, Expr right) {
-      this.operator = operator;
-      this.right = right;
-    }
-
-    final Token operator;
-    final Expr right;
-  }
+  abstract <R> R accept(Visitor<R> visitor);
 }
--- lox/src/main/java/com/craftinginterpreters/tool/GenerateAst.java
+++ lox/src/main/java/com/craftinginterpreters/tool/GenerateAst.java
@@ -14,10 +14,10 @@ public class GenerateAst {
         String outputDir = args[0];
 
         defineAst(outputDir, "Expr", Arrays.asList(
-                "Binary   : Expr left, Token operator, Expr right",
-                "Grouping : Expr expression",
                 "Literal  : Object value",
-                "Unary    : Token operator, Expr right"
+                "Unary    : Token operator, Expr right",
+                "Binary   : Expr left, Token operator, Expr right",
+                "Grouping : Expr expression"
         ));
     }
 
@@ -33,11 +33,16 @@ public class GenerateAst {
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
         for (String type : types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
             defineType(writer, baseName, className, fields);
         }
+
+        writer.println();
+        writer.println("  abstract <R> R accept(Visitor<R> visitor);");
 
         writer.println("}");
         writer.close();
@@ -57,10 +62,26 @@ public class GenerateAst {
             writer.println("    }");
             writer.println();
 
+            writer.println("    @Override");
+            writer.println("    <R> R accept(Visitor<R> visitor) {");
+            writer.println("      return visitor.visit" + className + baseName + "(this);");
+            writer.println("    }");
+
             for (String field : fields) {
                 writer.println("    final " + field + ";");
             }
 
             writer.println("  }");
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("  interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("  }");
     }
 }
