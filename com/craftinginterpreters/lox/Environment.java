76a8a1245bf27608763f95d78b1ea13811a231ac
JP! <joaobianchi@live.com>
2025-01-14 07:05:19 +0000

    :construction: chapter 11, resolving and binding (static scope)

--- com/craftinginterpreters/lox/Environment.java
+++ com/craftinginterpreters/lox/Environment.java
@@ -20,13 +20,32 @@ public class Environment {
             return values.get(name.lexeme);
         }
 
-        if (enclosing != null) return enclosing.get(name);
+        if (enclosing != null)
+            return enclosing.get(name);
 
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
 
     void define(String name, Object value) {
         values.put(name, value);
+    }
+
+    Environment ancestor(int distance) {
+        Environment environment = this;
+
+        for (int i = 0; i < distance; i++) {
+            environment = environment.enclosing;
+        }
+
+        return environment;
+    }
+
+    Object getAt(int distance, String name) {
+        return ancestor(distance).values.get(name);
+    }
+
+    void assignAt(int distance, Token name, Object value) {
+        ancestor(distance).values.put(name.lexeme, value);
     }
 
     void assign(Token name, Object value) {
